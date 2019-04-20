4234e348047c79e283e0a3ad2c1f6d752225ec58
Bowser65 <contact.bowser65@gmail.com>
2019-04-20 11:23:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# No Emote Colon
+
+Allows you to use emotes like on Twitch, without colons
---
+++ index.js
@@ -0,0 +1,28 @@
+const { Plugin } = require('powercord/entities');
+const { getModule } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+
+module.exports = class NoEmoteColon extends Plugin {
+  async startPlugin () {
+    // messages webpack module is gay, need to import it manually
+    const messages = await getModule([ 'sendMessage', 'editMessage', 'deleteMessage' ]);
+    const emojiStore = await getModule([ 'getGuildEmoji' ]);
+    const guildsStore = await getModule([ 'getSortedGuilds' ]);
+
+    inject('noEmoteColon-msg', messages, 'sendMessage', (args) => {
+      const emotes = guildsStore.getSortedGuilds().map(g => g.guild).map(g => emojiStore.getGuildEmoji(g.id)).flat();
+      args[1].content = args[1].content.split(' ').map(word => {
+        const emote = emotes.find(e => e.name === word);
+        if (emote) {
+          return `<${emote.animated ? 'a' : ''}:${emote.name}:${emote.id}>`;
+        }
+        return word;
+      }).join(' ');
+      return args;
+    }, true);
+  }
+
+  pluginWillUnload () {
+    uninject('noEmoteColon-msg');
+  }
+};
